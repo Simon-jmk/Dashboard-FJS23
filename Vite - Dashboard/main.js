b00e88e0d1ceacececacb8853491e19f5ac1700b
Simon-jmk <151679315+Simon-jmk@users.noreply.github.com>
2024-01-22 12:48:50 +0100

    Notebook function + editable Titel

--- Vite - Dashboard/main.js
+++ Vite - Dashboard/main.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+
+// CLOCK
 let time = document.getElementById("time");
 
 setInterval(() =>{
@@ -11,3 +13,39 @@ let date = document.getElementById("date");
 
 let dateFormat = { month: 'long', year: 'numeric', day: 'numeric' };
 date.innerHTML = new Date().toLocaleDateString('sv-SE', dateFormat);
+
+
+
+// NOTEBOARD
+document.addEventListener('DOMContentLoaded', function() {
+    const noteboardText = document.querySelector('.noteboard-text');
+
+    const savedNote = localStorage.getItem('savedNote');
+    if (savedNote) {
+        noteboardText.innerHTML = savedNote;
+    }
+
+    noteboardText.addEventListener('input', function() {
+        localStorage.setItem('savedNote', noteboardText.innerHTML);
+    });
+});
+
+// TITLE TEXT
+document.addEventListener('DOMContentLoaded', (event) => {
+    const savedTitle = localStorage.getItem('dashboardTitle');
+    const placeholderText = '...';
+
+    dashboardTitle.innerText = localStorage.getItem('dashboardTitle') || placeholderText;
+
+    dashboardTitle.addEventListener('blur', () => {
+        const titleText = dashboardTitle.innerText.trim() || placeholderText;
+        localStorage.setItem('dashboardTitle', titleText === placeholderText ? '' : titleText);
+        dashboardTitle.innerText = titleText;
+      });
+
+      dashboardTitle.addEventListener('focus', () => {
+        if (dashboardTitle.innerText === placeholderText) {
+          dashboardTitle.innerText = '';
+        }
+      });
+    });
